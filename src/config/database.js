a0bd683f3bb320d6b19e752fc6c4ede198f0f0a5
AbhijitSarker <abhijit.srker@gmail.com>
2025-08-02 02:24:07 +0600

    update MongoDB connection to include dbName option

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            dbName: process.env.DB_NAME,
+        });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
