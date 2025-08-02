58b336f79cd0cc6cebb6111605797679869a8be9
AbhijitSarker <abhijit.srker@gmail.com>
2025-08-03 05:55:42 +0600

    Disable xForwardedForHeader:  validation

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ app.use(morgan('dev'));
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 100, // limit each IP to 100 requests per windowMs
+    validate: {
+        xForwardedForHeader: false, // Disable this validation
+    },
 });
 app.use('/api', limiter);
 
